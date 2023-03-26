5621b33b31fce84303e8a7e2c5d06b32afbf79af
mooghoo <albertopablito8@gmail.com>
2023-03-26 11:48:38 -0400

    blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,30 @@
+import { graphql } from 'gatsby'
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul> {
+        data.allFile.nodes.map(node =>   (
+            <li key={node.name}>
+                {node.name}
+            </li>
+        )) }
+        </ul>
     </Layout>
   )
 }
 
+export const query = graphql`
+query MyQuery {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      nodes {
+        name
+      }
+    }
+  }`
 export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
