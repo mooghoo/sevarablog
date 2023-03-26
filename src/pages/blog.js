70460ce66839e936d899639585cea5c287a50907
mooghoo <albertopablito8@gmail.com>
2023-03-26 12:14:54 -0400

    mdx plugin

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,28 +3,38 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
+// using GraphQL query allFile > nodes with filter to blog 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul> {
-        data.allFile.nodes.map(node =>   (
-            <li key={node.name}>
-                {node.name}
-            </li>
+      {
+        data.allMdx.nodes.map(node =>   (
+            <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         )) }
-        </ul>
+      
     </Layout>
   )
 }
 
 export const query = graphql`
-query MyQuery {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+query {
+    allMdx(sort: {frontmatter: {date: DESC}}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }`
+
+
 export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
